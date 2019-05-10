ad272aae1b0370ff307edd5b5f576c38ce584181
mkhuthir <mkhuthir@gmail.com>
2019-05-10 18:22:06 +0400

    updated examples

--- keras/diabetes/model.py
+++ keras/diabetes/model.py
@@ -12,11 +12,8 @@ import tensorflow as tf
 # The following is to suppress TensorFlow Warnings
 tf.logging.set_verbosity(tf.logging.ERROR)
 
-# fix random seed for reproducibility
-seed = 7
-numpy.random.seed(seed)
-
 # load pima indians dataset
+print('\nLoading data from csv file')
 dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 
 # split into input (X) and output (Y) variables
@@ -24,19 +21,25 @@ X = dataset[:,0:8]
 Y = dataset[:,8]
 
 # create model
+print('Creating model')
 model = Sequential()
 model.add(Dense(12, input_dim=8, kernel_initializer='uniform', activation='relu'))
 model.add(Dense(8, kernel_initializer='uniform', activation='relu'))
 model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
 
 # Compile model
+print('Compiling Model')
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Fit the model
-history = model.fit(X, Y, validation_split=0.33, epochs=150, batch_size=10, verbose=0)
+print('Fitting Model')
+history = model.fit(X, Y, validation_split=0.33, epochs=200, batch_size=20, verbose=0)
 
 # list all data in history
-print(history.history.keys())
+print('History:',history.history.keys())
+
+# Print model summery
+print(model.summary())
 
 # summarize history for accuracy
 plt.plot(history.history['acc'])
