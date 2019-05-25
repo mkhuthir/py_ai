35937795b0c046e941534a2019162eaf73d22798
mkhuthir <mkhuthir@gmail.com>
2019-05-25 09:01:38 +0400

    added dlib examples

---
+++ dlib/face_similarity.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import face_recognition
+from pathlib import Path
+from PIL import Image
+
+# Load the image of the person we want to find similar people for
+known_image = face_recognition.load_image_file("./people/test_face.jpg")
+pil_image = Image.fromarray(known_image)
+pil_image.show()
+
+# Encode the known image
+known_image_encoding = face_recognition.face_encodings(known_image)[0]
+
+# Variables to keep track of the most similar face match we've found
+best_face_distance = 1.0
+best_face_image = None
+best_image_path = None
+
+# Loop over all the images we want to check for similar people
+for image_path in Path("people").glob("*.png"):
+    # Load an image to check
+    unknown_image = face_recognition.load_image_file(image_path)
+
+    # Get the location of faces and face encodings for the current image
+    face_encodings = face_recognition.face_encodings(unknown_image)
+
+    # Get the face distance between the known person and all the faces in this image
+    face_distance = face_recognition.face_distance(face_encodings, known_image_encoding)[0]
+    print("Checking: ", image_path, "Distance: ", face_distance)
+
+    # If this face is more similar to our known image than we've seen so far, save it
+    if face_distance < best_face_distance:
+        # Save the new best face distance
+        best_face_distance = face_distance
+        # Extract a copy of the actual face image itself so we can display it
+        best_image_path = image_path
+        best_face_image = unknown_image
+
+# Display the face image that we found to be the best match!
+print("Best Face Distance:",best_face_distance)
+print("Best Image path:", best_image_path)
+
+pil_image = Image.fromarray(best_face_image)
+pil_image.show()
