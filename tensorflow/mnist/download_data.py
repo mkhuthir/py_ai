08bf5b1324477ee75dec66ea8b216c8fb3d1fb47
mkhuthir <mkhuthir@gmail.com>
2019-05-25 11:36:36 +0400

    added datadownloader

---
+++ tensorflow/mnist/download_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -d data ]; then
+    echo "data directory already present"
+else
+    echo "creating folder /data"
+    mkdir data
+fi
+
+echo "Downloading data.."
+pushd data
+wget "https://s3.amazonaws.com/img-datasets/mnist.pkl.gz"
+popd
