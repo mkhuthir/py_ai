e880f9678b6dcf448fcf8cc73910b100fa9a49d0
mkhuthir <mkhuthir@gmail.com>
2024-02-24 20:40:46 +0400

    w

--- nvidia/vector_add.cu
+++ nvidia/vector_add.cu
@@ -7,21 +7,38 @@ void vector_add(float *out, float *a, float *b, int n) {
     }
 }
 
-void main(){
+int main(){
     float *a, *b, *out;
-    float *d_a;
+    float *d_a, *d_b, *d_out;
 
-    a = (float*)malloc(sizeof(float) * N);
+    // Allocate memory (CPU)
+    a   = (float*)malloc(sizeof(float) * N);
+    b   = (float*)malloc(sizeof(float) * N);
+    out = (float*)malloc(sizeof(float) * N);
 
-    // Allocate device memory for a
-    cudaMalloc((void**)&d_a, sizeof(float) * N);
+    // Allocate device memory (GPU)
+    cudaMalloc((void**)&d_a  , sizeof(float) * N);
+    cudaMalloc((void**)&d_b  , sizeof(float) * N);
+    cudaMalloc((void**)&d_out, sizeof(float) * N);
 
-    // Transfer data from host to device memory
-    cudaMemcpy(d_a, a, sizeof(float) * N, cudaMemcpyHostToDevice);
+    // Initialize array (CPU)
+    for(int i = 0; i < N; i++){
+        a[i] = 1.0f; b[i] = 2.0f;
+    }
 
-    vector_add<<<1,1>>>(out, d_a, b, N);
+    // Transfer data from host to device memory (CPU>GPU)
+    cudaMemcpy(d_a  , a,   sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b  , b,   sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_out, out, sizeof(float) * N, cudaMemcpyHostToDevice);
+
+    vector_add<<<1,1>>>(d_out, d_a, d_b, N);
 
     // Cleanup after kernel execution
     cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_out);
+
     free(a);
+    free(b);
+    free(out);
 }
