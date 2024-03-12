0dd70d1fdc0442687d517653ccbdd76676547338
mkhuthir <mkhuthir@gmail.com>
2024-03-12 12:07:02 +0400

    w

--- huggingface/chatbot.py
+++ huggingface/chatbot.py
@@ -4,10 +4,12 @@
 # Mar 2024
 
 from transformers.utils import logging
+from transformers import pipeline
+from transformers import Conversation
 
 logging.set_verbosity_error()
 
-from transformers import pipeline
+
 
 chatbot = pipeline(task="conversational",
                    model="./models/facebook/blenderbot-400M-distill")
@@ -16,14 +18,14 @@ user_message = """
 What are some fun activities I can do in the winter?
 """
 
-from transformers import Conversation
+
 conversation = Conversation(user_message)
 print(conversation)
 
 conversation = chatbot(conversation)
-
 print(conversation)
 
+# you need to add if you are expecting the bot to remember the old conversation
 conversation.add_message(
     {"role": "user",
      "content": """
@@ -34,6 +36,5 @@ What else do you recommend?
 print(conversation)
 
 conversation = chatbot(conversation)
-
 print(conversation)
 
