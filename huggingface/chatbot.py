2962ca7085be9769be3f5b0561162ce6a55dd2e1
mkhuthir <mkhuthir@gmail.com>
2024-03-12 12:08:13 +0400

    w

--- huggingface/chatbot.py
+++ huggingface/chatbot.py
@@ -3,13 +3,11 @@
 # Muthanna Alwahash
 # Mar 2024
 
-from transformers.utils import logging
-from transformers import pipeline
 from transformers import Conversation
+from transformers import pipeline
+from transformers.utils import logging
 
 logging.set_verbosity_error()
-
-
 
 chatbot = pipeline(task="conversational",
                    model="./models/facebook/blenderbot-400M-distill")
@@ -17,7 +15,6 @@ chatbot = pipeline(task="conversational",
 user_message = """
 What are some fun activities I can do in the winter?
 """
-
 
 conversation = Conversation(user_message)
 print(conversation)
