7cf7ff2c8cadc76f7a0e578f9476c70ca34189c6
mkhuthir <mkhuthir@gmail.com>
2024-03-12 12:30:45 +0400

    w

--- huggingface/chatbot.py
+++ huggingface/chatbot.py
@@ -10,7 +10,7 @@ from transformers.utils import logging
 logging.set_verbosity_error()
 
 chatbot = pipeline(task="conversational",
-                   model="./models/facebook/blenderbot-400M-distill")
+                   model="facebook/blenderbot-400M-distill")
 
 user_message = """
 What are some fun activities I can do in the winter?
