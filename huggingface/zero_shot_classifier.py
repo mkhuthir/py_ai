7d361378d5e3ff6c445cb596f8171f56b86c8b79
mkhuthir <mkhuthir@gmail.com>
2024-03-16 19:16:06 +0400

    w

---
+++ huggingface/zero_shot_classifier.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+
+# Muthanna Alwahash
+# Mar 2024
+
+from transformers import pipeline 
+from transformers.utils import logging
+import torch
+import gc
+
+logging.set_verbosity_error()
+
+
+
+# free memory and garbage collect
+# del xxxx
+gc.collect()
