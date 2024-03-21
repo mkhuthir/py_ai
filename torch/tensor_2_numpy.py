f8037a37524e18d1ac49a85965ed86c1c6163087
mkhuthir <mkhuthir@gmail.com>
2024-03-21 22:23:44 +0400

    w

--- torch/tensor_2_numpy.py
+++ torch/tensor_2_numpy.py
@@ -3,7 +3,7 @@
 import torch
 import numpy as np
 
-# Tendor to Numpy
+# Tensor to Numpy
 a = torch.ones(5)
 b = a.numpy()
 print(a)
--- torch/tensors.py
+++ torch/tensors.py
@@ -2,47 +2,46 @@
 
 import torch
 
-print("\n1D Tensors")
-
 # Create a Tensor with just ones in a column
 a = torch.ones(5)
+print("\n1D Tensors")
 print(a)
 
 # Create a Tensor with just zeros in a column
 b = torch.zeros(5)
+print("\n zeros in a column")
 print(b)
 
 # Create a Tensor from data
 c = torch.tensor([1.0, 2.0, 3.0, 4.0, 5.0])
+print("\na Tensor from data")
 print(c)
 
 # Create a 2D Tensor with just zeros 
-print("\n2D Tensors")
+print("\n2D Tensor with just zeros")
 d = torch.zeros(3,2)
 print(d)
 
 # Create a 2D Tensor with just ones
 e = torch.ones(3,2)
+print("\n2D Tensor with just ones")
 print(e)
 
 # Create a 2D Tensor with random values 
 f = torch.rand(5,5)
+print("\n2D Tensor with random values ")
 print(f)
 
 # Create a 3D Tensor with custom values
-print("\n3D Tensors")
 g = torch.tensor([
-                    [   [1, 2],
-                        [3, 4]
-                    ],
-
-                    [   [5, 6], 
-                        [7, 8]
-                    ]
+                    [   [1, 2], [3, 4]],
+                    [   [5, 6], [7, 8]]
                 ])
+print("\n3D Tensor with custom values")
 print(g)
  
 # Construct a 3x3x3 matrix, uninitialized:
+print("\n3x3x3 matrix, uninitialized")
 h = torch.empty(3, 3, 3)
 print(h)
 
