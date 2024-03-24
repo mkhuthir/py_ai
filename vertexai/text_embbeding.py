4fe8fa7d66c0a892eebf714a455b3db6e710db2b
mkhuthir <mkhuthir@gmail.com>
2024-03-24 22:10:17 +0400

    w

--- vertexai/text_embbeding.py
+++ vertexai/text_embbeding.py
@@ -29,3 +29,22 @@ embedding = embedding_model.get_embeddings(
 vector = embedding[0].values
 print(f"Length = {len(vector)}")
 print(vector[:10])
+
+# Calculate the similarity between two sentences as a number between 0 and 1.
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+emb_1 = embedding_model.get_embeddings(
+    ["What is the meaning of life?"]) # 42!
+
+emb_2 = embedding_model.get_embeddings(
+    ["How does one spend their time well on Earth?"])
+
+emb_3 = embedding_model.get_embeddings(
+    ["Would you like a salad?"])
+
+# Wrap the embeddings python list in another list because the cosine_similarity function expects either a 2D numpy array or a list of lists.
+vec_1 = [emb_1[0].values]
+vec_2 = [emb_2[0].values]
+vec_3 = [emb_3[0].values]
+
