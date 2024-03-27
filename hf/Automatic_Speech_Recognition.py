420fdbb4d5eb9b3a8d00e94bc2417a6051c66047
mkhuthir <mkhuthir@gmail.com>
2024-03-27 22:40:50 +0400

    w

--- hf/Automatic_Speech_Recognition.py
+++ hf/Automatic_Speech_Recognition.py
@@ -12,23 +12,23 @@ from datasets import load_dataset
 
 logging.set_verbosity_error()
 
-dataset = load_dataset("librispeech_asr",
-                       split="train.clean.100",
-                       streaming=True,
-                       trust_remote_code=True)
+#dataset = load_dataset("librispeech_asr",
+#                       split="train.clean.100",
+#                       streaming=True,
+#                       trust_remote_code=True)
 
-example = next(iter(dataset))
-dataset_head = dataset.take(5)
+#example = next(iter(dataset))
+#dataset_head = dataset.take(5)
 
 # print(list(dataset_head)[2])
 # print(example)
 
 asr = pipeline(task="automatic-speech-recognition",
-               model="./models/distil-whisper/distil-small.en",
+               model="distil-whisper/distil-small.en",
                device=0)
 
 print("asr sampling rate =",asr.feature_extractor.sampling_rate)
-print("example sampling rate",example['audio']['sampling_rate'])
+#print("example sampling rate",example['audio']['sampling_rate'])
 
-asr(example["audio"]["array"])
-print(example["text"])
+#asr(example["audio"]["array"])
+#print(example["text"])
