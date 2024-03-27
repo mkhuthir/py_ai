69db3fa336e3d3f62949d0c3efe8a10e72fe2b3d
mkhuthir <mkhuthir@gmail.com>
2024-03-27 22:22:32 +0400

    w

--- hf/Automatic_Speech_Recognition.py
+++ hf/Automatic_Speech_Recognition.py
@@ -3,6 +3,9 @@
 # Muthanna Alwahash
 # Mar 2024
 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
+
 from transformers import pipeline 
 from transformers.utils import logging
 from datasets import load_dataset
@@ -17,7 +20,15 @@ dataset = load_dataset("librispeech_asr",
 example = next(iter(dataset))
 dataset_head = dataset.take(5)
 
-print(list(dataset_head))
-print(list(dataset_head)[2])
-print(example)
+# print(list(dataset_head)[2])
+# print(example)
 
+asr = pipeline(task="automatic-speech-recognition",
+               model="./models/distil-whisper/distil-small.en",
+               device=0)
+
+print("asr sampling rate =",asr.feature_extractor.sampling_rate)
+print("example sampling rate",example['audio']['sampling_rate'])
+
+asr(example["audio"]["array"])
+print(example["text"])
