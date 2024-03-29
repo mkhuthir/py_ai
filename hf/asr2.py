a9c25129ce6681299ee3c028f7e2d1fb7c92fc98
mkhuthir <mkhuthir@gmail.com>
2024-03-29 22:26:59 +0400

    w

--- hf/asr2.py
+++ hf/asr2.py
@@ -3,13 +3,14 @@
 # Muthanna Alwahash
 # Mar 2024
 
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
-
 from transformers import pipeline 
 from transformers.utils import logging
 from datasets import load_dataset
+import soundfile as sf
+import io
+import os
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' # set tensorflow logs level
 logging.set_verbosity_error()
 
 asr = pipeline(task="automatic-speech-recognition",
@@ -18,3 +19,7 @@ asr = pipeline(task="automatic-speech-recognition",
 
 print("ASR sampling rate =",asr.feature_extractor.sampling_rate)
 
+audio, sampling_rate = sf.read('../media/narration2.mp3')
+
+print("Audio sampling rate =",sampling_rate)
+print("Audio shape =",audio.shape)
