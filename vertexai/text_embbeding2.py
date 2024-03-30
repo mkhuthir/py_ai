2ae733244112ced12de11c35405990de287dacb3
mkhuthir <mkhuthir@gmail.com>
2024-03-30 21:25:10 +0400

    w

---
+++ vertexai/text_embbeding2.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+
+# Muthanna Alwahash
+# Mar 2024
+from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
+
+#--------- Authenticate to Vertex AI
+key_path = '/home/mkhuthir/apps/vertexaiproj-418218-cf52d0c8ffb4.json'
+credentials = Credentials.from_service_account_file(key_path,
+                                                    scopes=['https://www.googleapis.com/auth/cloud-platform'])
+if credentials.expired:
+    credentials.refresh(Request())
+
+PROJECT_ID = 'vertexaiproj-418218'
+REGION = 'us-central1'
+#----------------------------------- 
+
+import vertexai
+from vertexai.language_models import TextEmbeddingModel
+
+# initialize vertex
+vertexai.init(project = PROJECT_ID, 
+              location = REGION, 
+              credentials = credentials)
+
