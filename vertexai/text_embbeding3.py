9cec98c3542ff0e6bfe00a3ee74aece7af9377eb
mkhuthir <mkhuthir@gmail.com>
2024-03-30 23:12:02 +0400

    w

--- vertexai/text_embbeding3.py
+++ vertexai/text_embbeding3.py
@@ -25,6 +25,8 @@ import mplcursors
 import numpy as np
 import pandas as pd
 import time
+from concurrent.futures import ThreadPoolExecutor
+from tqdm.auto import tqdm
 
 # initialize vertex
 vertexai.init(project = PROJECT_ID, 
@@ -36,24 +38,56 @@ print(so_df)
 
 model = TextEmbeddingModel.from_pretrained("textembedding-gecko@001")
 
+#------------
 # Generator function to yield batches of sentences
 def generate_batches(sentences, batch_size = 5):
     for i in range(0, len(sentences), batch_size):
         yield sentences[i : i + batch_size]
 
-so_questions = so_df[0:200].input_text.tolist() 
-batches = generate_batches(sentences = so_questions)
-
-batch = next(batches)
-print(len(batch))
-
+#------------
 def encode_texts_to_embeddings(sentences):
     try:
         embeddings = model.get_embeddings(sentences)
         return [embedding.values for embedding in embeddings]
     except Exception:
         return [None for _ in range(len(sentences))]
+#------------
+def encode_text_to_embedding_batched(sentences, api_calls_per_second = 0.33, batch_size = 5):
+    # Generates batches and calls embedding API
+    
+    embeddings_list = []
 
-batch_embeddings = encode_texts_to_embeddings(batch)
+    # Prepare the batches using a generator
+    batches = generate_batches(sentences, batch_size)
 
-print(f"{len(batch_embeddings)} embeddings of size {len(batch_embeddings[0])}")
+    seconds_per_job = 1 / api_calls_per_second
+
+    with ThreadPoolExecutor() as executor:
+        futures = []
+        for batch in tqdm(
+            batches, total = math.ceil(len(sentences) / batch_size), position=0
+        ):
+            futures.append(
+                executor.submit(functools.partial(encode_texts_to_embeddings), batch)
+            )
+            time.sleep(seconds_per_job)
+
+        for future in futures:
+            embeddings_list.extend(future.result())
+
+    is_successful = [
+        embedding is not None for sentence, embedding in zip(sentences, embeddings_list)
+    ]
+    embeddings_list_successful = np.squeeze(
+        np.stack([embedding for embedding in embeddings_list if embedding is not None])
+    )
+    return embeddings_list_successful
+
+so_questions = so_df.input_text.tolist()
+question_embeddings = encode_text_to_embedding_batched(
+                            sentences=so_questions,
+                            api_calls_per_second = 20/60, 
+                            batch_size = 5)
+
+print(f"{len(question_embeddings)} embeddings of size {len(question_embeddings[0])}")
+
