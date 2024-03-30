8eab3cbfa49fe14c0a83572a100e76f7dcc92ee3
mkhuthir <mkhuthir@gmail.com>
2024-03-30 09:28:30 +0400

    w

---
+++ hf/image_captioning2.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+
+# Muthanna Alwahash
+# Mar 2024
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' # set tensorflow logs level
+
+from transformers.utils import logging
+logging.set_verbosity_error()
+
+from transformers import pipeline
+from PIL import Image
+
+pipe = pipeline("image-to-text",
+                model="Salesforce/blip-image-captioning-base",
+                device=0)
+
+image = Image.open("../media/kittens.jpeg")
+image.show
+
+output=pipe(image)
+print(output[0]['generated_text'])
+
