5940c461433389f59613ef2ca2fabeb2e763cbfc
mkhuthir <mkhuthir@gmail.com>
2024-03-31 10:28:20 +0400

    w

--- vertexai/2K_embbed_classify.py
+++ vertexai/2K_embbed_classify.py
@@ -20,4 +20,18 @@ so_df = pd.read_csv('../media/so_database_app.csv')
 y = so_df['category'].values
 print("category data shape (y) = ",y.shape)
 
+# Split dataset to train and test
+X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                    y, 
+                                                    test_size = 0.2, 
+                                                    random_state = 2)
+
+# train
+clf = RandomForestClassifier(n_estimators=200)
+clf.fit(X_train, y_train)
+
+# check accuracy
+y_pred = clf.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
 
