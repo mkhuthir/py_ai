a2c6a7559bb6caeb26a3a5ad4511863d8acc588b
mkhuthir <mkhuthir@gmail.com>
2024-04-09 12:50:11 +0400

    w

---
+++ ollama/embeddings_mxbai.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+
+# Muthanna Alwahash
+# Mar 2024
+
+import ollama
+import time
+
+timer_start = time.time()
+#------------------------
+embedding = ollama.embeddings(model='mxbai-embed-large',
+                              prompt='Llamas are members of the camelid family')
+#------------------------
+timer_end = time.time()
+print(f"\ntime taken (ms): {1000 * (timer_end - timer_start)}\n\n")
+
+print(embedding)
